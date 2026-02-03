32a5fb396d74c2f8844cd688db4263a2c863f03c
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-02-03 23:56:49 +0600

    Update CoursesController.cs

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using UniversityManagementSystem.Data;
 using UniversityManagementSystem.Models;
-using System.Collections.Generic;
 
 namespace UniversityManagementSystem.Controllers
 {
