f27b89d7589aa8e001672ff256638cbc70468bfd
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-04-15 23:25:24 +0600

    Update ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ namespace UniversityManagementSystem.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Student> Students { get; set; }
-        public DbSet<Course> Courses { get; set; }
+        public DbSet<Course> Courses { get; set}
         public DbSet<Enrollment> Enrollments { get; set; }
     }
 }
